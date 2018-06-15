a25d151c09f201f28b42ce1634956177d23024af
tomasreichmann <tomasreichmann@gmail.com>
2018-06-15 13:16:15 +0200

    BUGFIX: RAIL-958 AttributeFilter onApply param structure is outdated

--- examples/src/components/AttributeElementsExample.jsx
+++ examples/src/components/AttributeElementsExample.jsx
@@ -1,6 +1,6 @@
 // (C) 2007-2018 GoodData Corporation
 import React, { Component } from 'react';
-import { AttributeElements } from '@gooddata/react-components';
+import { AttributeElements, ColumnChart } from '@gooddata/react-components';
 import PropTypes from 'prop-types';
 
 import '@gooddata/react-components/styles/css/main.css';
@@ -10,19 +10,20 @@ import { employeeNameIdentifier, projectId } from '../utils/fixtures';
 export class AttributeFilterItem extends Component {
     static propTypes = {
         title: PropTypes.string.isRequired,
-        uri: PropTypes.string.isRequired
+        uri: PropTypes.string.isRequired,
+        onChange: PropTypes.func.isRequired,
+        isSelected: PropTypes.bool.isRequired
     };
 
-    onChange(uri) {
-        // eslint-disable-next-line no-console
-        return event => console.log('AttributeFilterItem onChange', uri, event.target.value === 'on');
+    onChange = (uri) => {
+        return event => this.props.onChange(uri, event.target.value === 'on');
     }
 
     render() {
-        const { title, uri } = this.props;
+        const { title, uri, isSelected } = this.props;
         return (
             <label className="gd-list-item s-attribute-filter-list-item" style={{ display: 'inline-flex' }}>
-                <input type="checkbox" className="gd-input-checkbox" onChange={this.onChange(uri)} />
+                <input type="checkbox" checked={isSelected} className="gd-input-checkbox" onChange={this.onChange(uri)} />
                 <span>{title}</span>
             </label>
         );
@@ -30,9 +31,27 @@ export class AttributeFilterItem extends Component {
 }
 
 export class AttributeElementsExample extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: {}
+        };
+    }
+
+    onChange = (uri, isSelected) => {
+        this.setState({
+            selected: {
+                ...this.state.selected,
+                [uri]: isSelected
+            }
+        });
+    }
+
     render() {
+        const { selected } = this.state;
+
         return (
-            <div style={{ minHeight: 500 }}>
+            <div>
                 <AttributeElements identifier={employeeNameIdentifier} projectId={projectId} options={{ limit: 20 }}>
                     {({ validElements, loadMore, isLoading, error }) => {
                         const {
@@ -43,32 +62,66 @@ export class AttributeElementsExample extends Component {
                         if (error) {
                             return <div>{error}</div>;
                         }
+                        const selectedUris = Object.keys(selected).filter(key => !!selected[key]);
+                        const filters = selectedUris.length
+                            ? [{
+                                positiveAttributeFilter: {
+                                    displayForm: {
+                                        identifier: employeeNameIdentifier
+                                    },
+                                    in: selectedUris
+                                }
+                            }]
+                            : [];
                         return (
                             <div>
-                                <button
-                                    className="button button-secondary s-show-more-filters-button"
-                                    onClick={loadMore}
-                                    disabled={isLoading || (offset + count === total)}
-                                >More
-                                </button>
-                                <h2>validElements</h2>
-                                <pre>
-                                    isLoading: {isLoading.toString()}<br />
-                                    offset: {offset}<br />
-                                    count: {count}<br />
-                                    total: {total}<br />
-                                    nextOffset: {offset + count}
-                                </pre>
+                                <div style={{ height: 300, marginBottom: 20 }} >
+                                    <ColumnChart
+                                        projectId="xms7ga4tf3g3nzucd8380o2bev8oeknp"
+                                        measures={[{
+                                            measure: {
+                                                localIdentifier: 'totalSales',
+                                                definition: {
+                                                    measureDefinition: {
+                                                        item: {
+                                                            identifier: 'aa7ulGyKhIE5'
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }]}
+                                        filters={filters}
+                                    />
+                                </div>
                                 <div>
                                     {validElements ? validElements.items.map(item => (
                                         <AttributeFilterItem
                                             key={item.element.uri}
                                             uri={item.element.uri}
                                             title={item.element.title}
+                                            onChange={this.onChange}
+                                            isSelected={!!selected[item.element.uri]}
                                         />
                                     )) : null}
                                 </div>
-                                {validElements ? <pre>{JSON.stringify(validElements, null, '  ')}</pre> : null}
+                                <p>
+                                    <button
+                                        className="button button-secondary s-show-more-filters-button"
+                                        onClick={loadMore}
+                                        disabled={isLoading || (offset + count === total)}
+                                    >Load more
+                                    </button>
+                                </p>
+                                <div style={{ height: 300, overflow: 'auto' }} >
+                                    <pre>
+                                        isLoading: {isLoading.toString()}<br />
+                                        offset: {offset}<br />
+                                        count: {count}<br />
+                                        total: {total}<br />
+                                        nextOffset: {offset + count}
+                                    </pre>
+                                    {validElements ? <pre>{JSON.stringify(validElements, null, '  ')}</pre> : null}
+                                </div>
                             </div>
                         );
                     }}
--- examples/src/components/AttributeFilterExample.jsx
+++ examples/src/components/AttributeFilterExample.jsx
@@ -1,18 +1,42 @@
 // (C) 2007-2018 GoodData Corporation
 import React, { Component } from 'react';
-import { AttributeFilter } from '@gooddata/react-components';
+import { AttributeFilter, ColumnChart } from '@gooddata/react-components';
 
 import '@gooddata/react-components/styles/css/main.css';
 
 import { employeeNameIdentifier, projectId } from '../utils/fixtures';
 
 export class AttributeFilterExample extends Component {
-    onApply(params) {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filters: [],
+            names: [],
+            isPositive: false
+        };
+    }
+
+    onApply = (filter) => {
         // eslint-disable-next-line no-console
-        console.log('AttributeFilterExample onApply', ...params);
+        console.log('AttributeFilterExample filter', filter);
+
+        const isPositive = !!filter.in;
+        const elementsProp = isPositive ? 'in' : 'notIn';
+
+        const filters = [{
+            [isPositive ? 'positiveAttributeFilter' : 'negativeAttributeFilter']: {
+                displayForm: {
+                    uri: filter.id
+                },
+                [elementsProp]: filter[elementsProp].map(element => (`/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2200/elements?id=${element}`))
+            }
+        }];
+
+        this.setState({ filters, names: filter[elementsProp].map(element => element.title), isPositive });
     }
 
     render() {
+        const { filters, names, isPositive } = this.state;
         return (
             <div>
                 <AttributeFilter
@@ -20,7 +44,26 @@ export class AttributeFilterExample extends Component {
                     projectId={projectId}
                     fullscreenOnMobile={false}
                     onApply={this.onApply}
-                />
+                />&emsp;{(names.length && !isPositive && 'all employees except ') || null}{names.length ? names.join(', ') : 'all employees'}
+                <hr className="separator" />
+                <div style={{ height: 300 }}>
+                    <ColumnChart
+                        projectId="xms7ga4tf3g3nzucd8380o2bev8oeknp"
+                        measures={[{
+                            measure: {
+                                localIdentifier: 'totalSales',
+                                definition: {
+                                    measureDefinition: {
+                                        item: {
+                                            identifier: 'aa7ulGyKhIE5'
+                                        }
+                                    }
+                                }
+                            }
+                        }]}
+                        filters={filters}
+                    />
+                </div>
             </div>
         );
     }
